ac9ef504c89fcd6a9d7b60269ed0473dd6a1290e
HerlinaMubarok <49268468+HerlinaMubarok@users.noreply.github.com>
2023-01-02 11:37:54 +0700

    Add files via upload

---
+++ Menghitung luas dan keliling lingkaran.py
@@ -0,0 +1,13 @@
+#Menghitung luas lingkaran.
+print("Menghitung luas lingkaran")
+r = int(input("Masukkan jari-jari:"))
+phi = 3.14
+luas = phi*r*r
+print(luas)
+
+#Menghitung keliling lingkaran.
+print("Menghitung keliling lingkaran")
+d = int(input("Masukkan diameter:"))
+phi = 3.14
+keliling = phi*d
+print(keliling)
